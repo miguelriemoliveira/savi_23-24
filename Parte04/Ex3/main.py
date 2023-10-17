98a18daa913bf48bb21f35ac2fce957d851fe880
Miguel Oliveira <m.riem.oliveira@gmail.com>
2023-10-17 11:55:45 +0100

    Parte04 Examples from class

--- Parte04/Ex3/main.py
+++ Parte04/Ex3/main.py
@@ -4,12 +4,13 @@
 
 import copy
 import csv
+import math
 import time
 from random import randint
 
 import cv2
 import numpy as np
-from track import Detection, Track
+from track import Detection, Track, computeIOU
 from colorama import Fore, Back, Style
 
 
@@ -20,12 +21,15 @@ def main():
     # --------------------------------------
     cap = cv2.VideoCapture('../docs/OxfordTownCentre/TownCentreXVID.mp4')
 
-    file ='../docs/OxfordTownCentre/TownCentre-groundtruth.top'
-    gt_tracks = csv.reader(open(file))
 
     # Create person detector
     detector_filename = './fullbody2.xml' 
     detector = cv2.CascadeClassifier(detector_filename)
+
+    # Parameters
+    distance_threshold = 100
+    deactivate_threshold = 5.0 # secs
+    iou_threshold = 0.3
 
     video_frame_number = 0
     person_count = 0
@@ -40,6 +44,7 @@ def main():
         if result is False:
             break
 
+        frame_stamp = round(float(cap.get(cv2.CAP_PROP_POS_MSEC))/1000,2)
         height, width, _ = image_rgb.shape
         image_gui = copy.deepcopy(image_rgb) # good practice to have a gui image for drawing
 
@@ -58,11 +63,49 @@ def main():
         detection_idx = 0
         for x,y,w,h in haar_detections:
             detection_id = str(video_frame_number) + '_' +  str(detection_idx)
-            detection = Detection(x, x+w, y, y+h, detection_id)
+            detection = Detection(x, x+w, y, y+h, detection_id, frame_stamp)
             detections.append(detection)
             detection_idx += 1
 
-        # TODO How to handle when detection is from a person already being tracked?
+        all_detections = copy.deepcopy(detections)
+
+        # ------------------------------------------------------
+        # Association step. Associate detections with tracks
+        # ------------------------------------------------------
+        idxs_detections_to_remove = []
+        for idx_detection, detection in enumerate(detections):
+            for track in tracks:
+                if not track.active:
+                    continue
+                # --------------------------------------
+                # Using distance between centers
+                # --------------------------------------
+                # How to measure how close a detection is to a tracker?
+#                 distance = math.sqrt( (detection.cx-track.detections[-1].cx)**2 + 
+#                                       (detection.cy-track.detections[-1].cy)**2 )
+# 
+#                 if distance < distance_threshold: # This detection belongs to this tracker!!!
+#                     track.update(detection) # add detection to track
+#                     idxs_detections_to_remove.append(idx_detection)
+#                     break # do not test this detection with any other track
+
+                # --------------------------------------
+                # Using IOU
+                # --------------------------------------
+                iou = computeIOU(detection, track.detections[-1])
+                print('IOU( ' + detection.detection_id + ' , ' + track.track_id + ') = ' + str(iou))
+                if iou > iou_threshold: # This detection belongs to this tracker!!!
+                    track.update(detection) # add detection to track
+                    idxs_detections_to_remove.append(idx_detection)
+                    break # do not test this detection with any other track
+
+        idxs_detections_to_remove.reverse()
+
+        print('idxs_detections_to_remove ' + str(idxs_detections_to_remove))
+        for idx in idxs_detections_to_remove:
+            print(detections)
+            print('deleting detection idx ' + str(idx))
+            del detections[idx]
 
         # --------------------------------------
         # Create new trackers
@@ -73,24 +116,37 @@ def main():
             tracks.append(track)
             person_count += 1
 
-
+        # --------------------------------------
+        # Deactivate tracks if last detection has been seen a long time ago
+        # --------------------------------------
+        for track in tracks:
+            time_since_last_detection = frame_stamp - track.detections[-1].stamp
+            if time_since_last_detection > deactivate_threshold:
+                track.active = False
                
         # --------------------------------------
         # Visualization
         # --------------------------------------
 
-        # Draw list of detections
-        for detection in detections:
+        # Draw list of all detections (including those associated with the tracks)
+        for detection in all_detections:
             detection.draw(image_gui, (255,0,0))
 
         # Draw list of tracks
         for track in tracks:
+            if not track.active:
+                continue
             track.draw(image_gui)
 
 
+        if video_frame_number == 0:
+            cv2.namedWindow('GUI',cv2.WINDOW_NORMAL)
+            cv2.resizeWindow('GUI', int(width/2), int(height/2))
 
-        cv2.namedWindow('GUI',cv2.WINDOW_NORMAL)
-        cv2.resizeWindow('GUI', int(width/2), int(height/2))
+        # Add frame number and time to top left corner
+        cv2.putText(image_gui, 'Frame ' + str(video_frame_number) + ' Time ' + str(frame_stamp) + ' secs',
+                    (10,40), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,0), 2, cv2.LINE_AA)
+
         cv2.imshow('GUI',image_gui)
             
         if cv2.waitKey(0) & 0xFF == ord('q') :
--- Parte04/Ex3/track.py
+++ Parte04/Ex3/track.py
@@ -10,13 +10,48 @@ import cv2
 import numpy as np
 
 
+def computeIOU(d1, d2):
+    # box1 and box2 should be in the format (x1, y1, x2, y2)
+    x1_1, y1_1, x2_1, y2_1 = d1.left, d1.top, d1.right, d1.bottom
+    x1_2, y1_2, x2_2, y2_2 = d2.left, d2.top, d2.right, d2.bottom
+    
+    # Calculate the area of the first bounding box
+    area1 = (x2_1 - x1_1) * (y2_1 - y1_1)
+    
+    # Calculate the area of the second bounding box
+    area2 = (x2_2 - x1_2) * (y2_2 - y1_2)
+    
+    # Calculate the coordinates of the intersection
+    x1_i = max(x1_1, x1_2)
+    y1_i = max(y1_1, y1_2)
+    x2_i = min(x2_1, x2_2)
+    y2_i = min(y2_1, y2_2)
+    
+    # Check if there is an intersection
+    if x1_i < x2_i and y1_i < y2_i:
+        # Calculate the area of the intersection
+        area_i = (x2_i - x1_i) * (y2_i - y1_i)
+        
+        # Calculate the area of the union
+        area_u = area1 + area2 - area_i
+        
+        # Calculate IoU
+        iou = area_i / area_u
+        return iou
+    else:
+        return 0.0
+
+
 class Detection():
-    def __init__(self, left, right, top, bottom, id):
+    def __init__(self, left, right, top, bottom, id, stamp):
         self.left = left
         self.right = right
         self.top = top
         self.bottom = bottom
-        self.detector_id = id
+        self.cx = int((left + right)/2)
+        self.cy = int((top + bottom)/2)
+        self.detection_id = id
+        self.stamp = stamp
 
     def draw(self, image, color, draw_position='bottom', text=None):
         start_point = (self.left, self.top)
@@ -24,14 +59,18 @@ class Detection():
         cv2.rectangle(image, start_point, end_point, color, 3)
 
         if text is None:
-            text = 'Det ' + self.detector_id
+            text = 'Det ' + self.detection_id
 
         if draw_position == 'bottom':
             position = (self.left, self.bottom + 30)
         else:
             position = (self.left, self.top-10)
 
+
         cv2.putText(image, text, position, cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2, cv2.LINE_AA)
+
+        # Draw center of box
+        cv2.line(image, (self.cx, self.cy), (self.cx, self.cy), color, 3) 
 
     def getLowerMiddlePoint(self):
         return (self.left + int((self.right - self.left)/2) , self.bottom)
@@ -41,16 +80,17 @@ class Track():
 
     # Class constructor
     def __init__(self, id, detection,  color=(255, 0, 0)):
-        self.tracker_id = id
+        self.track_id = id
         self.color = color
         self.detections = [detection]
+        self.active = True
 
-        print('Starting constructor for track id ' + str(self.tracker_id))
+        print('Starting constructor for track id ' + str(self.track_id))
 
     def draw(self, image):
 
         #Draw only last detection
-        self.detections[-1].draw(image, self.color, text=self.tracker_id, draw_position='top')
+        self.detections[-1].draw(image, self.color, text=self.track_id, draw_position='top')
 
         for detection_a, detection_b in zip(self.detections[0:-1], self.detections[1:]):
             start_point = detection_a.getLowerMiddlePoint()
@@ -58,12 +98,6 @@ class Track():
             cv2.line(image, start_point, end_point, self.color, 3) 
 
 
-    def update(self, left, right, top, bottom):
+    def update(self, detection):
+        self.detections.append(detection)
 
-        self.detections.append(Detection(left,right, top, bottom))
-
-#     def __repr__(self):
-#         left, right, top, bottom  = self.detections[-1] # Get the last know position, i.e. last detection in the list of detections
-# 
-#         return 'track ' + str(self.id) + ' ndets=' + str(len(self.detections)) + ' l=' + str(left) + ' r=' + str(right) + ' t=' + str(top) + ' b=' + str(bottom)
-#         
