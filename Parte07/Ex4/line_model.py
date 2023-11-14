34decf9a041c62abe6c2e48932bc05897d64b10f
Miguel Oliveira <m.riem.oliveira@gmail.com>
2023-11-14 12:01:36 +0000

    Ex4

--- Parte07/Ex4/line_model.py
+++ Parte07/Ex4/line_model.py
@@ -7,6 +7,53 @@ import numpy as np
 import statistics as stat
 
 
+class ParabModel():
+
+    def __init__(self):
+
+        # Using model
+        # y = a(x-h)2 + k
+
+        self.a = 2
+        self.h = 2
+        self.k = 0
+        self.handle = None
+
+    def computeError(self, xs, ys_ground_truth):
+
+        ys = self.getYs(xs)
+
+        total_error = 0
+        for y, y_ground_truth in zip(ys, ys_ground_truth):
+            error = abs(y_ground_truth-y)
+            total_error += error
+
+        return total_error
+
+    def randomizeParams(self):
+        self.a = random.uniform(-2, 2)
+        self.h = random.uniform(-15, 15)
+        self.k = random.uniform(-15, 15)
+
+    def getYs(self, xs):
+        ys = []
+        for x in xs:
+            ys.append(self.a * (x - self.h)**2 + self.k)
+
+        return ys
+
+    def draw(self, fig, color='b'):
+
+        xs = list(np.linspace(0, 10, num=200))
+        ys = self.getYs(xs)
+
+        if self.handle is None:  # draw first time
+            self.handle = fig.plot(xs, ys, '-' + color)
+
+        else:  # edit plot all other times
+            fig.setp(self.handle, xdata=xs, ydata=ys)
+
+
 class LineModel():
 
     def __init__(self):
@@ -39,7 +86,7 @@ class LineModel():
 
     def draw(self, fig, color='b'):
 
-        xs = list(np.linspace(-10, 10, num=2))
+        xs = list(np.linspace(-10, 0, num=2))
         ys = self.getYs(xs)
 
         if self.handle is None:  # draw first time
--- Parte07/Ex4/main.py
+++ Parte07/Ex4/main.py
@@ -5,7 +5,7 @@ import random
 from colorama import Fore, Style
 import matplotlib.pyplot as plt
 import numpy as np
-from line_model import LineModel
+from line_model import LineModel, ParabModel
 
 from scipy.optimize import least_squares
 
@@ -17,18 +17,33 @@ def main():
     # --------------------------------------
 
     # Setup matplotlib figure
-    plt.title('Ex2', fontweight="bold")
+    plt.title('E42', fontweight="bold")
     plt.axis([-10, 10, -5, 5])
 
     # Load the points from file
     with open('points.pkl', 'rb') as f:
         points = pickle.load(f)
 
+    xs_line = []
+    ys_line = []
+    xs_parab = []
+    ys_parab = []
+
+    for x, y in zip(points['xs'], points['ys']):
+        if x <= 0:
+            xs_line.append(x)
+            ys_line.append(y)
+        else:
+            xs_parab.append(x)
+            ys_parab.append(y)
+
     # Draw points on figure
-    plt.plot(points['xs'], points['ys'], 'rx')
+    plt.plot(xs_line, ys_line, 'rx')
+    plt.plot(xs_parab, ys_parab, 'gx')
 
     # Define a line model
     line = LineModel()
+    parab = ParabModel()
 
     # --------------------------------------
     # Execution
@@ -38,13 +53,16 @@ def main():
 
         line.m = params[0]
         line.b = params[1]
+        parab.a = params[2]
+        parab.h = params[3]
+        parab.k = params[4]
 
-        error = line.computeError(
-            xs=points['xs'], ys_ground_truth=points['ys'])
-        print('error = ' + str(error))
+        error_line = line.computeError(xs=xs_line, ys_ground_truth=ys_line)
+        error_parab = parab.computeError(xs=xs_parab, ys_ground_truth=ys_parab)
 
-        # Draw new line
-        line.draw(plt)
+        # Draw models
+        line.draw(plt, color='r')
+        parab.draw(plt, color='g')
 
         # Draw figure
         plt.draw()
@@ -52,16 +70,14 @@ def main():
         if pressed_key == True:
             exit(0)
 
-        return error
+        return error_line + error_parab
 
-    params0 = [line.m, line.b]
+    params0 = [line.m, line.b, parab.a, parab.h, parab.k]
     result = least_squares(objectiveFunction, params0, verbose=2)
 
     print(result)
 
     # draw final solution
-    line.m = result.x[0]
-    line.b = result.x[1]
 
     line.draw(plt, color='g')
     plt.draw()
